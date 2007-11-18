8574a25f2be8cca5ba7ae0494fde01af8d03f011
Jose.R.Fonseca <devnull@localhost>
2007-11-18 23:38:02 +0000

    New test. Oprofile tests.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,24 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 PYTHON=python2.5
 
 TESTDIR=`dirname "$0"`
 GPROF2DOT=$TESTDIR/../gprof2dot.py
 
-for FORMAT in prof pstats
+if [ "$1" ]
+then
+	FORMATS=$@
+else
+	FORMATS="prof pstats oprofile"
+fi
+
+for FORMAT in $FORMATS
 do
 	for INPUT in $TESTDIR/*.$FORMAT
 	do
 		NAME=${INPUT%%.$FORMAT}
 		OUTPUT=$NAME.dot
-		echo $INPUT
-		if ! $PYTHON $GPROF2DOT -f $FORMAT -o $OUTPUT $INPUT
-		then
-			echo FAILED
-		else
-			# TODO: regression testing
-			dot -Tpng -o $NAME.png $OUTPUT
-			echo OK
-		fi
+		echo $PYTHON $GPROF2DOT -f $FORMAT -o $OUTPUT $INPUT
+		$PYTHON $GPROF2DOT -f $FORMAT -o $OUTPUT $INPUT || continue
+		echo dot -Tpng -o $NAME.png $OUTPUT
+		dot -Tpng -o $NAME.png $OUTPUT || continue
 	done
 done
