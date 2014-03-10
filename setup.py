44832b15e8391a3d0d3524d11b562a632f5935a6
José Fonseca <jose.r.fonseca@gmail.com>
2014-03-10 13:59:32 +0000

    Add setup.py to upload package to pypi.
    Per googlecode issue #91

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# The purpose of this script is to enable uploading gprof2dot.py to the Python
+# Package Index, which can be easily done by doing:
+#
+#   python setup.py register
+#   python setup.py sdist upload
+#
+# See also:
+# - https://code.google.com/p/jrfonseca/issues/detail?id=19
+# - http://docs.python.org/2/distutils/packageindex.html
+#
+
+import time
+import sys
+from setuptools import setup
+
+setup(
+    name='gprof2dot',
+    version=time.strftime('%Y.%m.%d'),
+    author='Jose Fonseca',
+    author_email='jose.r.fonseca@gmail.com',
+    url='https://code.google.com/p/jrfonseca/wiki/Gprof2Dot',
+    description="Generate a dot graph from the output of several profilers.",
+    long_description="""
+        gprof2dot.py is a Python script to convert the output from many
+        profilers into a dot graph.
+        """,
+    license="LGPL",
+
+    py_modules=['gprof2dot'],
+    entry_points=dict(console_scripts=['gprof2dot=gprof2dot:main']),
+)
