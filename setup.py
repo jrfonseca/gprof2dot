733b59379592c39d9f595de7323260d397b8d3b9
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-10-24 14:00:16 +0500

    Deprecated: python 2.6
    Explicitly defined python 2.7 as only python2 supported version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
 
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
 
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
